e019e2b66ab4cb322d62851a20f0075869f7824c
Shadman Sakib <shadmansakib018@gmail.com>
2025-04-05 17:25:27 -0500

    push before trying python inside java

---
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/TestPython.java
@@ -0,0 +1,14 @@
+package org.cloudbus.cloudsim.examples;
+
+public class TestPython {
+
+	public TestPython() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
